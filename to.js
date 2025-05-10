828a2888fc7cdffbfb15316d969a0d6311247b4e
abdsheikh12 <muhammadabdullahansari1719@gmail.com>
2025-05-10 11:38:16 -0700

    Update to.js

--- to.js
+++ to.js
@@ -39,3 +39,13 @@ function show() {
 }
 
 show();
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js')
+    .then(function(registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch(function(error) {
+      console.log('Service Worker registration failed:', error);
+    });
+}
+
